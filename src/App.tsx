1702afaead63360eb2b223f4173fdfa6d27a2c80
ReihaneB <reihaneb@liome.fr>
2022-10-01 17:32:16 +0200

    First commit !

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div>
+        <h2 className="text-center mb-2">Bienvenue</h2>
+        <h1 className="text-center">Reihane B.</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
