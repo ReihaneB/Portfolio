03c9aa936829ee1dc4e9cb1d797d363b190b7031
ReihaneB <reihaneb@liome.fr>
2022-10-02 16:35:17 +0200

    modify h1 & h2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,11 @@ module.exports = {
   plugins: [
     plugin(function ({ addBase, theme }) {
       addBase({
-        h1: { fontSize: theme("fontSize.2xl") },
-        h2: { fontSize: theme("fontSize.xl") },
+        h1: { fontSize: theme("fontSize.3xl") },
+        h2: {
+          fontSize: theme("fontSize.2xl"),
+          fontWeight: theme("fontWeight.medium"),
+        },
         h3: { fontSize: theme("fontSize.lg") },
       });
     }),
