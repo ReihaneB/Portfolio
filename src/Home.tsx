6d8d020357cd7d9fac4e74e04f705b34efaf723e
ReihaneB <reihaneb@liome.fr>
2022-10-02 18:07:34 +0200

    add project list

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,35 @@ import Logo from "./components/Logo";
 import reactLogo from "./assets/react.svg";
 import profilePicture from "./assets/profile-picture.png";
 import quarry from "./assets/decors-quarry.png";
+import liomewebsite from "./assets/liome-website.png";
+import ultrabook from "./assets/ultrabook.png";
+
+const projectArr = [
+  {
+    projectName: "Liome",
+    jobTitle: "Frontend Developer",
+    stack: {
+      stackSummary: ["React.Js", "Recoil", "TailwindCSS", "Firebase"],
+    },
+    firstImage: liomewebsite,
+  },
+  {
+    projectName: "Liome",
+    jobTitle: "3D Artist",
+    stack: {
+      stackSummary: ["Blender", "Photoshop", "Panther 3D"],
+    },
+    firstImage: quarry,
+  },
+  {
+    projectName: "Ultrabook",
+    jobTitle: "Développeur Front-End · Webdesigner",
+    stack: {
+      stackSummary: ["HTML", "PHP", "Figma"],
+    },
+    firstImage: ultrabook,
+  },
+];
 
 export default function Home() {
   return (
@@ -39,12 +68,16 @@ export default function Home() {
 https://www.linkedin.com/in/reihaneb/"
             target="_blank"
             rel="noreferrer"
-            className="text-blue-500"
+            className="text-blue-500 bg-blue-100 rounded-full px-4 py-2 mr-2"
           >
             LinkedIn
           </a>
-          <span className="mx-2">|</span>
-          <a href="" target="_blank" rel="noreferrer" className="text-blue-500">
+          <a
+            href=""
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 bg-blue-100 rounded-full px-4 py-2"
+          >
             GitHub
           </a>
         </p>
@@ -60,24 +93,35 @@ https://www.linkedin.com/in/reihaneb/"
       </div>
 
       <div className="mt-32">
-        <hr className="text-gray-400 border-gray-400 overflow-visible text-center after:content-['1'] after:bg-white after:p-4 after:relative after:top-[-13px]" />
-        <div className="mt-32">
-          <h2 className="">Nom du projet</h2>
-          <p className="">Intitulé du poste</p>
-          <div className="flex flex-wrap">
-            <p className="mt-4 bg-gray-200 px-6 py-2 rounded-xl w-fit mr-4">
-              Blender
-            </p>
-            <p className="mt-4 bg-gray-200 px-6 py-2 rounded-xl w-fit mr-4">
-              Photoshop
-            </p>
-            <p className="mt-4 bg-gray-200 px-6 py-2 rounded-xl w-fit">
-              Panther 3D
-            </p>
+        {projectArr.map((project, index: number) => (
+          <div key={index}>
+            <div className="flex flex-nowrap justify-center items-center my-32">
+              <hr className=" border-gray-400 mr-4 w-1/2" />
+              <span className="text-gray-400">{index + 1}</span>
+              <hr className=" border-gray-400 ml-4 w-1/2" />
+            </div>
+            <div className="">
+              <h2 className="">{project.projectName}</h2>
+              <p className="">{project.jobTitle}</p>
+              <div className="flex flex-wrap">
+                {project.stack.stackSummary.map((stack, index) => (
+                  <p
+                    key={index}
+                    className="mt-4 bg-gray-200 px-6 py-2 rounded-xl w-fit mr-4"
+                  >
+                    {stack}
+                  </p>
+                ))}
+              </div>
+              <img
+                src={project.firstImage}
+                alt="Quarry"
+                className="mt-6 w-full rounded-xl"
+              />
+            </div>
           </div>
-          <img src={quarry} alt="Quarry" className="mt-6 w-full rounded-xl" />
-        </div>
-        <hr className=" mt-32 text-gray-400 border-gray-400 overflow-visible text-center after:content-['1'] after:bg-white after:p-4 after:relative after:top-[-13px]" />
+        ))}
+        <hr className="my-32 border-gray-400" />
       </div>
     </div>
   );
