98f441ae1310805899b359836266091ab8f90b44
ReihaneB <reihaneb@liome.fr>
2022-10-03 17:37:10 +0200

    add workorlab button in the bottom homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -35,100 +35,100 @@ const projectArr = [
 
 export default function Home() {
   return (
-    <div className="mx-16">
-    <div className="sticky top-[700px] z-10">
-    <WorkOrLab />
-    </div>
-      <div className="mt-12 flex justify-center ">
-        <Logo />
+    <>
+      <div className="fixed bottom-8 w-full px-16">
+        <WorkOrLab />
       </div>
-      <p className="mt-36 text-3xl text-center">
-        Bonjour ! Je suis
-        <br />
-        Reihane B.
-        <br />
-        <b>Développeur Front-end</b>
-      </p>
-      <div className="flex justify-center mt-6">
-        <img src={reactLogo} className="animate-spin" alt="React logo" />
-      </div>
-      <div className="mt-6">
-        <button className="w-full max-w-96 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full  py-3 text-white">
-          Démarrer un projet
-        </button>
-      </div>
-
-      <div className="flex justify-center mt-36">
-        <img
-          src={profilePicture}
-          alt="Profile picture of Reihane B."
-          className="rounded-xl w-full"
-        />
-      </div>
-      <div className="">
-        <p className="mt-6 text-center">
-          <a
-            href="
-https://www.linkedin.com/in/reihaneb/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-500 bg-blue-100 rounded-full px-4 py-2 mr-2"
-          >
-            LinkedIn
-          </a>
-          <a
-            href=""
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-500 bg-blue-100 rounded-full px-4 py-2"
-          >
-            GitHub
-          </a>
+      <div className="mx-16">
+        <div className="mt-12 flex justify-center ">
+          <Logo />
+        </div>
+        <p className="mt-36 text-3xl text-center">
+          Bonjour ! Je suis
+          <br />
+          Reihane B.
+          <br />
+          <b>Développeur Front-end</b>
         </p>
+        <div className="flex justify-center mt-6">
+          <img src={reactLogo} className="animate-spin" alt="React logo" />
+        </div>
         <div className="mt-6">
-          <h2 className="">Jewelry, front web development, luxurytech</h2>
-          <p className="mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id
-            ut sollicitudin facilisi. Nibh ornare ac adipiscing laoreet. Euismod
-            justo sagittis, non pellentesque vitae. Mauris faucibus proin nisi,
-            faucibus.
+          <button className="w-full max-w-96 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full  py-3 text-white">
+            Démarrer un projet
+          </button>
+        </div>
+
+        <div className="flex justify-center mt-36">
+          <img
+            src={profilePicture}
+            alt="Profile picture of Reihane B."
+            className="rounded-xl w-full"
+          />
+        </div>
+        <div className="">
+          <p className="mt-6 text-center">
+            <a
+              href="
+https://www.linkedin.com/in/reihaneb/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 bg-blue-100 rounded-full px-4 py-2 mr-2"
+            >
+              LinkedIn
+            </a>
+            <a
+              href=""
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 bg-blue-100 rounded-full px-4 py-2"
+            >
+              GitHub
+            </a>
           </p>
+          <div className="mt-6">
+            <h2 className="">Jewelry, front web development, luxurytech</h2>
+            <p className="mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              id ut sollicitudin facilisi. Nibh ornare ac adipiscing laoreet.
+              Euismod justo sagittis, non pellentesque vitae. Mauris faucibus
+              proin nisi, faucibus.
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-32">
+          {projectArr.map((project, index: number) => (
+            <div key={index}>
+              <div className="flex flex-nowrap justify-center items-center my-32">
+                <hr className=" border-gray-400 mr-4 w-1/2" />
+                <span className="text-gray-400">{index + 1}</span>
+                <hr className=" border-gray-400 ml-4 w-1/2" />
+              </div>
+              <div className="">
+                <h2 className="">{project.projectName}</h2>
+                <p className="">{project.jobTitle}</p>
+                <div className="flex flex-wrap">
+                  {project.stack.stackSummary.map((stack, index) => (
+                    <p
+                      key={index}
+                      className="mt-4 bg-gray-200 px-6 py-2 rounded-xl w-fit mr-4"
+                    >
+                      {stack}
+                    </p>
+                  ))}
+                </div>
+                <img
+                  src={project.firstImage}
+                  alt="Quarry"
+                  className="mt-6 w-full rounded-xl"
+                />
+              </div>
+            </div>
+          ))}
+          <hr className="my-36 border-gray-400" />
         </div>
       </div>
-
-      <div className="mt-32">
-        {projectArr.map((project, index: number) => (
-          <div key={index}>
-            <div className="flex flex-nowrap justify-center items-center my-32">
-              <hr className=" border-gray-400 mr-4 w-1/2" />
-              <span className="text-gray-400">{index + 1}</span>
-              <hr className=" border-gray-400 ml-4 w-1/2" />
-            </div>
-            <div className="">
-              <h2 className="">{project.projectName}</h2>
-              <p className="">{project.jobTitle}</p>
-              <div className="flex flex-wrap">
-                {project.stack.stackSummary.map((stack, index) => (
-                  <p
-                    key={index}
-                    className="mt-4 bg-gray-200 px-6 py-2 rounded-xl w-fit mr-4"
-                  >
-                    {stack}
-                  </p>
-                ))}
-              </div>
-              <img
-                src={project.firstImage}
-                alt="Quarry"
-                className="mt-6 w-full rounded-xl"
-              />
-            </div>
-          </div>
-        ))}
-        <hr className="my-32 border-gray-400" />
-        <span className="flex justify-center">Reihane B.</span>
-        
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/WorkOrLab.tsx
+++ src/components/WorkOrLab.tsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
 
-export default function WorkOrLab () { 
+export default function WorkOrLab() {
+  const [isToggle, setIsToggle] = useState<boolean>(false);
   return (
-    <div className="bg-gray-600 rounded-full w-full h-16 grid grid-cols-2">
-    <div className="flex justify-center items-center my-4 ml-4 bg-gray-200 rounded-l-full">Work</div>
-    <div className="flex justify-center items-center my-4 mr-4 bg-gray-200 rounded-r-full ">Lab</div>
-    </div>
-  )
-} 
+    <>
+      <nav className="bg-gray-700 rounded-full flex justify-center items-center z-10">
+        <ul className="bg-gray-500 w-full flex flex-nowrap relative rounded-full m-4">
+          <input
+            className="hidden"
+            type="radio"
+            name="workOrLab"
+            id="work"
+            onChange={() => setIsToggle(false)}
+            defaultChecked
+          />
+          <label
+            htmlFor="work"
+            className={` ${
+              isToggle === false ? `text-black` : `text-white`
+            } z-20 text-center grow py-3 duration-500`}
+          >
+            Work
+          </label>
+
+          <input
+            className="hidden"
+            type="radio"
+            name="workOrLab"
+            id="lab"
+            onChange={() => setIsToggle(true)}
+          />
+          <label
+            htmlFor="lab"
+            className={` ${
+              isToggle === true ? `text-black` : `text-white`
+            } z-20 text-center grow py-3 duration-500`}
+          >
+            Lab
+          </label>
+
+          <a
+            className={`w-1/2 h-full absolute bg-white rounded-full transform transition-all duration-500 ${
+              isToggle === false ? `left-0` : `left-1/2`
+            }`}
+          ></a>
+        </ul>
+      </nav>
+    </>
+  );
+}
