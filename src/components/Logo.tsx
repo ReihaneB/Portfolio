17db95dfa4a01b450c9f1dafaeaba3205efd8018
ReihaneB <reihaneb@liome.fr>
2022-10-16 17:21:46 +0200

    logo size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo(): JSX.Element {
   return (
-    <div className="h-24 w-24 border-solid border-2 border-[#89db84] rounded-full flex justify-center items-center duration-500">
+    <div className="h-20 w-20 border-solid border-2 border-[#89db84] rounded-full flex justify-center items-center duration-500">
       <h1>rb.</h1>
     </div>
   );
