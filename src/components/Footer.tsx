98520f3ecda306e9a8dde6d17eef377522df4d02
ReihaneB <reihaneb@liome.fr>
2022-10-22 18:14:00 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,28 +8,6 @@ export default function Footer(): JSX.Element {
 
   return (
     <footer>
-<<<<<<< Updated upstream
-      <h1 className="text-center">rb.</h1>
-
-      <p className="mt-6 text-center">
-        <a
-          href="
-https://www.linkedin.com/in/reihaneb/"
-          target="_blank"
-          rel="noreferrer"
-          className="text-blue-500 bg-blue-100 rounded-full px-4 py-2 mr-2"
-        >
-          LinkedIn
-        </a>
-        <a
-          href=""
-          target="_blank"
-          rel="noreferrer"
-          className="text-blue-500 bg-blue-100 rounded-full px-4 py-2"
-        >
-          GitHub
-        </a>
-=======
       {isContactFormOpen && (
         <ContactForm close={() => setIsContactFormOpen(!isContactFormOpen)} />
       )}
@@ -72,7 +50,6 @@ https://www.linkedin.com/in/reihaneb/"
             </a>
           ))
         )}
->>>>>>> Stashed changes
       </p>
     </footer>
   );
