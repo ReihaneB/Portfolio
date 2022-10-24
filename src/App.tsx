4df1c8c3838a34b6546775b3145e6169e98eacc7
ReihaneB <reihaneb@liome.fr>
2022-10-24 16:40:09 +0200

    add scrollToTop in transition page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Error404 from "./pages/Error404";
 import Project from "./pages/Project";
+import ScrollToTop from "./hooks/useScrollToTop";
 
 export default function App() {
   return (
     <div className="mt-12 lg:flex lg:justify-center mb-36">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/:id" element={<Home />} />
-          <Route path="*" element={<Error404 />} />
+          <Route path="*" element={<Home />} />
           <Route path="/project/:id" element={<Project />} />
           <Route path="/about" element={<About />} />
         </Routes>
